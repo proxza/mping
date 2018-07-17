6004363aebfc88d2cca1b0e2de8638a80dc6ec25
proxz <proxza@gmail.com>
2018-07-17 15:40:33 +0300

    Uploading rep

---
+++ src/mping/MPing.java
@@ -0,0 +1,17 @@
+
+package mping;
+
+/**
+ *
+ * @author proxz
+ */
+public class MPing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mPingGUI start = new mPingGUI();
+    }
+    
+}
